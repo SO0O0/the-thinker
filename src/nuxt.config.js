069d0babbdf70f9d16b8790feafa1009164bdf46
SouHirawata <hirawatasou@gmail.com>
2022-12-24 20:41:59 +0900

    :sparkles: Introduce PixelMplus

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -20,6 +20,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    { src: '~/assets/fonts.scss', lang: 'scss' },
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
