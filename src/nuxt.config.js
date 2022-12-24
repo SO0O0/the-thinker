47d26c9405afe0ff5b7853ca8e921bb74803ac8e
SouHirawata <hirawatasou@gmail.com>
2022-12-24 21:20:26 +0900

    :rocket: Test launch

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -47,5 +47,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  router: {
+    base: '/the-thinker/'
+  },
+
+  generate: {
+    dir: '../docs'
+  },
 }
